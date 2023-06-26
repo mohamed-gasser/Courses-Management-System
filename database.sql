c7c6d38931c103aa1c304b59d99df7cb851800f1
Mohamed Gasser Mohamed <102036714+mohamed-gasser@users.noreply.github.com>
2023-06-27 00:21:32 +0300

    Add files via upload

---
+++ database.sql
@@ -0,0 +1,4 @@
+select name from course join stu_cr on course.id=stu_cr.cr_id where stu_cr.st_id=201900610;
+select * from course_details join course on course.id=course_details.cr_id;
+sad
+'asdasdsd
